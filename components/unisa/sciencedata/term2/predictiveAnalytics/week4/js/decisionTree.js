2daa0450552e79573d84f101cf8e636d46b59b3f
Jet <nutterair1989@gmail.com>
2023-04-02 10:26:06 +0930

    add decision tree example for predictive analytics

---
+++ components/unisa/sciencedata/term2/predictiveAnalytics/week4/js/decisionTree.js
@@ -0,0 +1,12 @@
+Vue.config.productionTip = false
+baseSlide.data.src = [
+  './img/L4_basic_classification_页面_06.jpg',
+  './img/L4_basic_classification_页面_07.jpg',
+  './img/L4_basic_classification_页面_08.jpg',
+  './img/L4_basic_classification_页面_09.jpg',
+  './img/L4_basic_classification_页面_10.jpg',
+  './img/L4_basic_classification_页面_11.jpg',
+  './img/L4_basic_classification_页面_12.jpg',
+  './img/L4_basic_classification_页面_13.jpg',
+]
+new Vue(baseSlide)
