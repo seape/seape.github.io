77012322242ccf7e0d57f9c041a1ed232249c74a
Jet <nutterair1989@gmail.com>
2023-04-02 13:45:32 +0930

    add multiple comparision procedure

---
+++ components/unisa/sciencedata/term2/predictiveAnalytics/week5/js/MulCompProc.js
@@ -0,0 +1,8 @@
+Vue.config.productionTip = false
+baseSlide.data.src = [
+  './img/L5_overfitting__页面_12.jpg',
+  './img/L5_overfitting__页面_13.jpg',
+  './img/L5_overfitting__页面_14.jpg',
+  './img/L5_overfitting__页面_15.jpg',
+]
+new Vue(baseSlide)
