ab7ae58fa42a92610037b6f9bc800135e6a5826d
Jet <nutterair1989@gmail.com>
2023-04-02 13:49:25 +0930

    fix multiple comparison procedure

--- components/unisa/sciencedata/term2/predictiveAnalytics/week5/js/MulCompProc.js
+++ components/unisa/sciencedata/term2/predictiveAnalytics/week5/js/MulCompProc.js
@@ -1,8 +1,7 @@
 Vue.config.productionTip = false
 baseSlide.data.src = [
-  './img/L5_overfitting__页面_12.jpg',
-  './img/L5_overfitting__页面_13.jpg',
-  './img/L5_overfitting__页面_14.jpg',
-  './img/L5_overfitting__页面_15.jpg',
+  './img/L5_overfitting__页面_20.jpg',
+  './img/L5_overfitting__页面_21.jpg',
+  './img/L5_overfitting__页面_22.jpg'
 ]
 new Vue(baseSlide)
