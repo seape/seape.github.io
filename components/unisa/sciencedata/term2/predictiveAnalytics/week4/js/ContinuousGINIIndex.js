4de28acb3872c24e9e83e2265fbfbf6b410c0f69
Jet <nutterair1989@gmail.com>
2023-04-02 11:03:16 +0930

    add computing continuous GINI index

---
+++ components/unisa/sciencedata/term2/predictiveAnalytics/week4/js/ContinuousGINIIndex.js
@@ -0,0 +1,9 @@
+Vue.config.productionTip = false
+baseSlide.data.src = [
+  './img/L4_basic_classification_页面_39.jpg',
+  './img/L4_basic_classification_页面_40.jpg',
+  './img/L4_basic_classification_页面_41.jpg',
+  './img/L4_basic_classification_页面_42.jpg',
+  './img/L4_basic_classification_页面_43.jpg',
+]
+new Vue(baseSlide)
