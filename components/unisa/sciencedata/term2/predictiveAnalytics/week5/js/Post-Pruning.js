84282232fd8ed3675d1874a9dd374999590447f0
Jet <nutterair1989@gmail.com>
2023-04-02 14:59:29 +0930

    add Post-Pruning

---
+++ components/unisa/sciencedata/term2/predictiveAnalytics/week5/js/Post-Pruning.js
@@ -0,0 +1,7 @@
+Vue.config.productionTip = false
+baseSlide.data.src = [
+  './img/L5_overfitting__页面_35.jpg',
+  './img/L5_overfitting__页面_36.jpg',
+  './img/L5_overfitting__页面_37.jpg'
+]
+new Vue(baseSlide)
